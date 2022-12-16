48aa23d2912eb7a5c620e62276fa528a57e8100f
UdayKajana <59999909+UdayKajana@users.noreply.github.com>
2022-12-16 14:55:05 +0530

    Create PascalTriangle.java

---
+++ PascalTriangle.java
@@ -0,0 +1,20 @@
+package Patterns;
+import java.util.Scanner;
+public class PascalTriangle {
+    public static void main(String argvs[]){
+        Scanner scanner=new Scanner(System.in);
+        int a= scanner.nextInt();
+        for(int i=0;i<a;i++){
+            for(int j=0;j<a;j++){
+                if(j<a-i)
+                System.out.print(" ");
+                else
+                System.out.print("*");
+                if(j<a)
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
