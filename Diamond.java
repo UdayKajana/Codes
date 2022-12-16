36bcf55caa2dba4e56c0db522a9064c0abd52b7b
UdayKajana <59999909+UdayKajana@users.noreply.github.com>
2022-12-16 14:52:03 +0530

    Create Diamond.java

---
+++ Diamond.java
@@ -0,0 +1,19 @@
+package Patterns;
+import java.util.Scanner;
+public class Diamond {
+    public static void main(String argvs[]){
+        Scanner scanner=new Scanner(System.in);
+        int a= scanner.nextInt();
+        int c=(a*2)-1;
+        for(int i=0;i<c;i++){
+            for(int j=0;j<c;j++){
+                if(j<Math.abs(a-i-1) || j>(a+i-1) || (i>=a && j>=(c-i%a-1)))
+                System.out.print("   ");
+                else
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+        scanner.close();
+    }
+}
