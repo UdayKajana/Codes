93d55548fd961aad327d60671f09c566d0c14949
UdayKajana <kajanauday@Gmail.com>
2022-12-20 06:15:12 +0000

    Commit from gitpod

--- Pyramid.java
+++ Pyramid.java
@@ -4,14 +4,15 @@ public class Pyramid {
     public static void main(String argvs[]){
         Scanner scanner=new Scanner(System.in);
         int a= scanner.nextInt();
+        int count=1;
         for(int i=0;i<a;i++){
             for(int j=0;j<a;j++){
                 if(j<a-i-1)
-                System.out.print("_");  
+                System.out.print(" ");  
                 else{
-                System.out.print("*");
+                System.out.print("asdfghjkl;");
                 if(j<a-1)
-                System.out.print("_");
+                System.out.print(" ");
                 }
             }
             System.out.println();
