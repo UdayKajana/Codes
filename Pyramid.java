4fe5b7f278ba843596aadcd4d245a18821bf5b06
UdayKajana <kajanauday@Gmail.com>
2022-12-22 17:23:27 +0000

    prints Pyramid 22/12/22

--- Pyramid.java
+++ Pyramid.java
@@ -1,23 +1,17 @@
 package Patterns;
 import java.util.Scanner;
-public class Pyramid {
+public class PascalTriangle {
     public static void main(String argvs[]){
         Scanner scanner=new Scanner(System.in);
         int a= scanner.nextInt();
-        int count=0;
         for(int i=0;i<a;i++){
-            count=1;
             for(int j=0;j<a;j++){
-                if(j<a-i-1)
-                System.out.print(" ");  
-                else{
-                    if(count<5)
-                    System.out.print(count++);
-                    else
-                    System.out.print(count--);
-                if(j<a-1)
-                    System.out.print(" ");
-                }
+                if(j<a-i)
+                System.out.print(" ");
+                else
+                System.out.print("*");
+                if(j<a)
+                System.out.print(" ");
             }
             System.out.println();
         }
