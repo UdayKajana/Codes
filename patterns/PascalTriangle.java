fead048b6da84da42a30fa2f5be4e21b999e22f8
UdayKajana <kajanauday@Gmail.com>
2023-01-30 09:15:26 +0000

    commit

--- patterns/PascalTriangle.java
+++ patterns/PascalTriangle.java
@@ -4,21 +4,19 @@ public class PascalTriangle {
     public static void main(String argvs[]){
         Scanner scanner=new Scanner(System.in);
         int a= scanner.nextInt();
-        int count=1;
-        boolean flag=true;
+        int half=0;
+        boolean flag=true;        
+        int p=0;
         for(int i=0;i<a;i++){
-            count=1;
-            flag=true;
+            p=1;
             for(int j=0;j<a;j++){
                 if(j<a-i-1)
                 System.out.print(" ");
                 else{
-                    if(count==(i+3)/2)
-                    flag=false;
-                    if(flag)
-                    System.out.print(count++);
+                    if(j==a)
+                    System.out.print(j);
                     else
-                    System.out.print(count--);
+                    System.out.print(j);
                     if(j<a-1)
                     System.out.print(" ");
                 }
