a65b2ad6af02883383de775ce7cd27fbde5b467f
UdayKajana <kajanauday@Gmail.com>
2023-02-19 06:00:33 +0000

    commit

--- CyclicShift.py
+++ CyclicShift.py
@@ -5,9 +5,9 @@ def solve(s):
     place = 0
     for i in range(len(s)-1,-1,-1):
         sum = 0
-        temp = s[:]
+        temp = s[i+1:]
         temp = temp+s[0:i+1]
-        getDecimal(temp)
+        print(temp)
 
 T = int(input())
 while (T > 0):
