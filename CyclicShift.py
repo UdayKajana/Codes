346a02f15611ec8dbb7b3ba09e61c8790755c29b
UdayKajana <kajanauday@Gmail.com>
2023-02-20 16:34:49 +0000

    commit

--- CyclicShift.py
+++ CyclicShift.py
@@ -1,24 +1,31 @@
-def getDecimal(temp):
-    p=0
-    for i in range(len(temp)):
-        p=p+pow(2, i)
-    print(reversed(temp),p)
-    print("")
+def getDecimal(l):
+    l = list(reversed(l))
+    mult = 1
+    sum = 0
+    for i in range(len(l)):
+        sum = sum+(int(l[i])*mult)
+        mult = mult*2
+    return sum
 
-def solve(s):
-    l = list()
-    place = 0
-    for i in range(len(s)-1,-1,-1):
-        sum = 0
-        temp = s[i+1:]
-        temp = temp+s[0:i+1]
-        getDecimal(temp)
 
 T = int(input())
 while (T > 0):
+    sums = list()
+    pos = list()
     l = list()
     T = T-1
     n, k = map(int, input().split())
     s = input()
-    s=s.replace(" ","")
-    solve(s)
+    s = s.replace(" ", "")
+    sums.append(getDecimal(s))
+    for i in range(len(s)):
+        sums.append(getDecimal(s[i+1:]+s[0:i+1]))
+    mx=max(sums)
+    for i in range(len(sums)):
+        if sums[i] == mx:
+            if len(pos)==0:
+                pos.append(i)
+            else:
+                pos.append(pos[-1]-i)
+    print(pos)
+    
