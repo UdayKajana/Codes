9fb75dd496a2a0f0103692683659849a213cf660
UdayKajana <kajanauday@Gmail.com>
2023-02-27 16:07:45 +0000

    commit

--- leetcode/MaxProfit.java
+++ leetcode/MaxProfit.java
@@ -1,18 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min=prices[0],index=0;
+        int max=prices[0],index=0,min=prices[0];
         for(int i=0;i<prices.length;i++){
-            if(prices[i]<min){
-            min=prices[i];
-            index=i;
+            if(prices[i]>max){
+                
             }
-        }
-        int max=min;
-        for(int i=index+1;i<prices.length;i++){
-            if(max<prices[i]){
-                max=prices[i];
-            }
-        }        
+        }     
         return max-min;
     }
 }
